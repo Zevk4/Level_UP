fd63697493590107101fdd2f1cfb37244bdb6366
Zevk4 <gi.andrades@duocuc.cl>
2025-09-03 01:17:40 -0400

    Mejorando Front, cambios al CSS,JS y HTML  v1.02

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 // Base de datos en memoria xD faltan productos si
 const productos = [
     { codigo: 'JM001', categoria: 'Juegos de Mesa', nombre: 'Catan', precio: 29990, descripcion: 'Un clásico juego de estrategia...' },
@@ -56,3 +58,60 @@ document.addEventListener('DOMContentLoaded', () => {
         megaMenu.style.display = 'none';
     });
 });
+
+
+// Para las diferentes imagenes del Slider pero aun no esta listo XDDASDAD EN DESARROLLO O si me puedes ayudar a completarlo
+
+/*
+
+const images = [
+    {
+        src: "",
+        alt: "Imagen del control de Xbox"
+    },
+    {
+        src: "",
+        alt: "Segunda imagen "
+    },
+    {
+        src: "",
+        alt: "Tercera imagen "
+    }
+];
+
+    let currentIndex = 0;
+    const sliderImage = document.getElementById("slider-image");
+    const prevBtn = document.getElementById("prev-btn");
+    const nextBtn = document.getElementById("next-btn");
+
+    function showImage(index) {
+        sliderImage.src = images[index].src;
+        sliderImage.alt = images[index].alt;
+    }
+
+    prevBtn.addEventListener("click", () => {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        showImage(currentIndex);
+    });
+
+    nextBtn.addEventListener("click", () => {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    });
+
+
+function initHeartPath(){
+    heart = [];
+    const PI2 = 6.28318;
+    const steps = Math.max(32, config.particleCount);
+
+    for (let i = 0; i < steps; 1++){
+        const t = (i / steps) = PI2;
+        heart.push([
+            canvasW
+        ])
+    }
+}
+
+
+*/
